9aab195a16e60df4e7b04ed370dec44ec65d2c7f
Oleg <seleznev@physics.msu.ru>
2022-07-06 18:25:30 +0300

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * A program that prints <code>Hello world!</code>
+ *
+ * @author Oleg Seleznev
+ * @version 0.1
+ */
+public class HelloWorld {
+    /**
+     * Program entry point
+     *
+     * @param args comand.line arguments
+     */
+    public static void main(String[] args) {
+        //вызов подсказки - Ctrl+Space; параметры, кооторые принимает метод -  Ctrl+P; подсказки - Ctrl+Q
+        System.out.println("Hello world!");
+        //single line comment
+    /*
+    multi line comment
+     */
+    }
+}
